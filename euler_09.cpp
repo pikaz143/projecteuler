cba07d8840b331d08922d14bf8281deedbd2f742
pikaz143 <42296778+pikaz143@users.noreply.github.com>
2018-09-04 23:58:45 +0530

    program 9

---
+++ euler_09.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+unsigned long int p=0;
+void pythagoreanTriplets(long int limit)
+{
+    long int a, b, c = 0;
+    long int m = 2;
+    while (c < limit)
+        {
+        for ( long int n = 1; n < m; ++n)
+            {
+            a = m * m - n * n;
+            b = 2 * m * n;
+            c = m * m + n * n;
+
+            if (c > limit)
+                break;
+            if((a+b+c)==1000)
+            p=a*b*c;
+           }
+        m++;
+    }
+}
+int main()
+{
+     long int limit = 998;
+    pythagoreanTriplets(limit);
+    printf("%lu",p);
+    return 0;
+}
