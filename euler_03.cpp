b0d6d7f9f067fd3571997dd8dcad28bf09dac087
pikaz143 <42296778+pikaz143@users.noreply.github.com>
2018-09-05 00:28:44 +0530

    Update euler_03.cpp

--- euler_03.cpp
+++ euler_03.cpp
@@ -1,29 +1,45 @@
-#include<iostream>
-using namespace std;
+#include <stdio.h>
+#include <math.h>
+
+#define NUMBER 600851475143
+
+int isPrime(int test)
+{
+    int k=0;
+    int s=( (int) sqrt(test) );
+
+    for(k = 3; k <= s; k+=2)
+    {
+        if(test % k == 0)
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
 
 int main()
 {
-   const unsigned long long int num=600851475;
-    unsigned long long int mx=0;
-    for(unsigned long long int i=2;i<num;i++)
+    int j=0;
+    int k=0;
+
+
+    while(1)
     {
-        if(num%i==0)
+        for(k=3; ; k+=2)
         {
-            bool flag=true;
-            for(unsigned long long int j=2;j<i;j++)
+            if(isPrime(k))
             {
-                if(i%j==0)
-                {
-                    flag=false;
-                    break;
-                }
+                if(NUMBER%k==0)
+                    j=k;
+                if(k>=(sqrt(NUMBER)))
+                    goto END;
             }
-            if(flag)
-                mx=i;
         }
-
     }
-    cout<<"largest prime factor= "<<mx;
 
+    END:
+    printf("%d", j);
     return 0;
 }
