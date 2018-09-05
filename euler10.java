ccf444d19b8abb3478eeabacc353dcb835ed7750
pikaz143 <42296778+pikaz143@users.noreply.github.com>
2018-09-05 22:55:09 +0530

    q.10 java

---
+++ euler10.java
@@ -0,0 +1,27 @@
+
+public class euler10 {
+
+	public static void main(String[] args) {
+	    int flag=0;
+	    long s=0;
+	     long l=2000000;
+	    for(long i=3;i<l;i+=2)
+	    {
+	     flag=0;
+	        for(long j=2;j<=i/2;j++)
+	       {
+	        if(i%j==0)
+	        {
+	          flag=1;
+	           break;
+	        }
+	       }
+	    if(flag==0)
+	    s=s+i;
+	    }
+
+	    System.out.println(s);
+
+	}
+
+}
